020e23200cbd703927ab1b5ccce90a9fb581c0be
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-11-18 11:16:55 -0500

    making tableview option to list it out as data

---
+++ FourSquareProj/FavoriteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionViewController.swift
+//  FourSquareProj
+//
+//  Created by EricM on 11/7/19.
+//  Copyright © 2019 EricM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let fave = [Fave]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
